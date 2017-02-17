d6865b74d6835633c0113cba049d3158ec63616c
Nail Gimaev <gimntut@list.ru>
2017-02-17 14:48:53 +0500

    Revert "v0.2 Init"
    This reverts commit 4b39e7f60b1aa7a1e3e2a5093d5c24f97e7a31c1.

--- nail_ssg/plugins/pages.py
+++
@@ -1,8 +0,0 @@
-from .abstract_plugin import Plugin
-
-class Pages(Plugin):
-	"""docstring for Pages"""
-	_config = None
-	def __init__(self, config):
-		super(Pages, self).__init__()
-		self._config = config
